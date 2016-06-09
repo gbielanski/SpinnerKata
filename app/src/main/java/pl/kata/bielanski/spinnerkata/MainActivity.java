eaa5fcd0bd737fe9543e5cf216fdd13a8705ad6d
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-06-09 10:59:33 +0200

    show Toast on item selected

--- app/src/main/java/pl/kata/bielanski/spinnerkata/MainActivity.java
+++ app/src/main/java/pl/kata/bielanski/spinnerkata/MainActivity.java
@@ -2,8 +2,11 @@ package pl.kata.bielanski.spinnerkata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
 	@Bind(R.id.spinner)
 	Spinner spinner;
+	private ArrayAdapter<String> adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,18 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		ButterKnife.bind(this);
 		String [] items = getResources().getStringArray(R.array.list_of_items);
-		ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+		adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
 		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				Toast.makeText(MainActivity.this, adapter.getItem(position), Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+
+			}
+		});
 	}
 }
